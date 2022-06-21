b1bc7fe7743fab6914bbc4e51421cee15dc2800b
Shingai Shamu <mshamu95@gmail.com>
2022-06-21 15:06:48 +0200

    Added execute sync logic

---
+++ census_blueprints/execute_sync.py
@@ -0,0 +1,77 @@
+import argparse
+import sys
+import requests
+import shipyard_utils as shipyard
+
+
+EXIT_CODE_INVALID_CREDENTIALS = 200
+EXIT_CODE_BAD_REQUEST = 201
+EXIT_CODE_SYNC_REFRESH_ERROR = 210
+EXIT_CODE_UNKNOWN_ERROR = 211
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--access-token', dest='access_token', required=True)
+    parser.add_argument('--sync-id', dest='sync_id', required=True)
+    args = parser.parse_args()
+    return args
+
+
+def execute_sync(sync_id, access_token):
+    """
+    Executes/starts a Census Sync
+    """
+    sync_post_api = f"https://bearer:{access_token}@app.getcensus.com/api/v1/syncs/{sync_id}/trigger"
+    api_headers = {
+        'Content-Type' : 'application/json'
+        }
+    try:
+        sync_trigger_response = requests.post(sync_post_api, headers=api_headers)
+        # check if successful, if not return error message
+        if sync_trigger_response.status_code in [200, 201]:
+            sync_trigger_json = sync_trigger_response.json()
+        else:
+            print(f"Sync request failed. Reason: {sync_trigger_response.content}")
+            sys.exit(EXIT_CODE_BAD_REQUEST)
+    except Exception as e:
+        print(f"Sync trigger request failed due to: {e}")
+        sys.exit(EXIT_CODE_BAD_REQUEST)
+
+    if sync_trigger_json['status'] == 'success':
+        print("Successfully triggered sync")
+        return sync_trigger_response.json()
+
+    elif sync_trigger_json['status'] == 'error':
+       print(f"Encounted an error - Census says: {sync_trigger_json['message']}")
+       sys.exit(EXIT_CODE_SYNC_REFRESH_ERROR)
+    else:
+        print(f"An unknown error has occured - API response: {sync_trigger_json}")
+        sys.exit(EXIT_CODE_UNKNOWN_ERROR)
+
+
+
+def main():
+    args = get_args()
+    access_token = args.access_token
+    sync_id = args.sync_id
+    
+    # execute trigger sync
+    trigger_sync = execute_sync(sync_id, access_token)
+    sync_run_id = trigger_sync['data']['sync_run_id']
+    
+    # create artifacts folder to save run id
+    base_folder_name = shipyard.logs.determine_base_artifact_folder(
+        'census')
+    artifact_subfolder_paths = shipyard.logs.determine_artifact_subfolders(
+        base_folder_name)
+    shipyard.logs.create_artifacts_folders(artifact_subfolder_paths)
+    
+    # save sync run id as variable
+    shipyard.logs.create_pickle_file(artifact_subfolder_paths, 
+                                'sync_run_id', sync_run_id)
+
+if __name__ == "__main__":
+    main()
+
+
